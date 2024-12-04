71e1fa983591a977c46283970e52bea5cd7d07c7
Amvisha <amvisha@gmail.com>
2024-12-05 01:58:00 +0200

    1

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
 print("Hello World")
 print("Hello Git")
+#name = ‘Wendy//’s dog’
+print("Wendy\\’s dog")
+var1 = 12 + 34
+print(var1)
+var2 = ['hi', 'hello', 'ola']
+print(len(var2))
+menu_items = ['wrap', 'salad', 'sandwich', 'soup']
+print(menu_items)
+def greet():
+    return 'Hello'
+print(greet(), 'Wendy')
+my_lucky_number = 7
+guess = int(input('Guess my lucky number! I think it is: '))
+while my_lucky_number != guess:
+     guess = int(input('Oops! Not it. Try again: '))
+print('Congrats! You guessed it.')
