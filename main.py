59d217c051653ed47890f64bbe0b2046be4fe789
Amvisha <amvisha@gmail.com>
2024-12-11 00:43:54 +0200

    third commit

--- main.py
+++ main.py
@@ -1,18 +1,4 @@
-print("Hello World")
-print("Hello Git")
-#name = ‘Wendy//’s dog’
-print("Wendy\\’s dog")
-var1 = 12 + 34
-print(var1)
-var2 = ['hi', 'hello', 'ola']
-print(len(var2))
-menu_items = ['wrap', 'salad', 'sandwich', 'soup']
-print(menu_items)
-def greet():
-    return 'Hello'
-print(greet(), 'Wendy')
-my_lucky_number = 7
-guess = int(input('Guess my lucky number! I think it is: '))
-while my_lucky_number != guess:
-     guess = int(input('Oops! Not it. Try again: '))
-print('Congrats! You guessed it.')
+list1 = ["зелене", "стигла", "червоний"]
+list2 = ["яблуко", "вишня", "томат"]
+for number, letter in zip(list1, list2):
+    print(number, letter)
