b69b21daeb732bfee79486174a71bb32729788da
Amvisha <amvisha@gmail.com>
2024-12-31 17:31:28 +0200

    9

--- while1.py
+++ while1.py
@@ -6,5 +6,12 @@ while num > 0:
     sum += num
     num -= 1
 """
+
 def invite_to_event(username):
-    return f"Dear {username}, we have the honour to invite you to our event"
+    return print(f"""Dear {username}, we have the honour to invite you to our event
+Dear {username}, we have the honour to invite you to our event
+Dear {username}, we have the honour to invite you to our event""")
+invite_to_event('Stepan')
+user = 'Ivan'
+print(f'{user[3]}')
+invite_to_event(user)
