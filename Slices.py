e7dd8390e07a5d9de2c96022572786b09591616f
Amvisha <amvisha@gmail.com>
2025-01-12 20:12:25 +0200

    9

--- Slices.py
+++ Slices.py
@@ -283,6 +283,112 @@ timestamp = 1617183600
 # Конвертація timestamp назад у datetime
 dt_object = datetime.fromtimestamp(timestamp)
 print(dt_object)  # Виведе відповідний datetime
+"""
+%Y - рік з чотирма цифрами (наприклад, 2023).
+%y - рік з двома цифрами (наприклад, 23).
+%m - місяць як номер (наприклад, 03 для березня).
+%d - день місяця як номер (наприклад, 14).
+%H - година (24-годинний формат) (наприклад, 15).
+%I - година (12-годинний формат) (наприклад, 03).
+%M - хвилини (наприклад, 05).
+%S - секунди (наприклад, 09).
+%A - повна назва дня тижня (наприклад, Tuesday).
+%a - скорочена назва дня тижня (наприклад, Tue).
+%B - повна назва місяця (наприклад, March).
+%b або %h - скорочена назва місяця (наприклад, Mar).
+%p - AM або PM для 12-годинного формату."""
+from datetime import datetime
+now = datetime.now()
+# Форматування дати і часу
+formatted_date = now.strftime("%Y-%m-%d %H:%M:%S")
+print(formatted_date)
+# Форматування лише дати
+formatted_date_only = now.strftime("%A, %d %B %Y")
+print(formatted_date_only)
+# Форматування лише часу
+formatted_time_only = now.strftime("%I:%M %p")
+print(formatted_time_only)
+# Форматування лише дати
+formatted_date_only = now.strftime("%d.%m.%Y")
+print(formatted_date_only)
+from datetime import datetime
+# Припустимо, у нас є дата у вигляді рядка
+date_string = "2023.03.14"
+# Перетворення рядка в об'єкт datetime
+datetime_object = datetime.strptime(date_string, "%Y.%m.%d")
+print(datetime_object)  # Виведе об'єкт datetime, що відповідає вказаній даті та часу
+from datetime import datetime
+# Поточна дата та час
+now = datetime.now()
+# Конвертація у формат ISO 8601
+iso_format = now.isoformat()
+print(iso_format)
+from datetime import datetime
+iso_date_string = "2023-03-14T12:39:29.992996"
+# Конвертація з ISO формату
+date_from_iso = datetime.fromisoformat(iso_date_string)
+print(date_from_iso)
+from datetime import datetime
+# Створення об'єкта datetime
+now = datetime.now()
+# Використання isoweekday() для отримання дня тижня
+day_of_week = now.isoweekday()
+print(f"Сьогодні: {day_of_week}")  # Поверне число від 1 до 7, що відповідає дню тижня
+from datetime import datetime
+# Створення об'єкта datetime
+now = datetime.now()
+# Отримання ISO календаря
+iso_calendar = now.isocalendar()
+print(f"ISO рік: {iso_calendar[0]}, ISO тиждень: {iso_calendar[1]}, ISO день тижня: {iso_calendar[2]}")
+from datetime import datetime, timezone
+local_now = datetime.now()
+utc_now = datetime.now(timezone.utc)
+print(local_now)
+print(utc_now)  # Виведе поточний час в UTC
+from datetime import datetime, timezone, timedelta
+utc_time = datetime.now(timezone.utc)
+# Створення часової зони для Східного часового поясу (UTC-5)
+eastern_time = utc_time.astimezone(timezone(timedelta(hours=-5)))
+# Перетворює час UTC в час Східного часового поясу
+print(eastern_time)
+from datetime import datetime, timezone, timedelta
+# Припустимо, місцевий час належить до часової зони UTC+2
+local_timezone = timezone(timedelta(hours=2))
+local_time = datetime(year=2025, month=1, day=12, hour=20, minute=00, second=0, tzinfo=local_timezone)
+# Конвертація локального часу в UTC
+utc_time = local_time.astimezone(timezone.utc)
+print(utc_time)  # Виведе час в UTC
+from datetime import datetime, timezone, timedelta
+# Час у конкретній часовій зоні
+timezone_offset = timezone(timedelta(hours=2))  # Наприклад, UTC+2
+timezone_datetime = datetime(year=2023, month=3, day=14, hour=12, minute=30, second=0, tzinfo=timezone_offset)
+# Конвертація у формат ISO 8601
+iso_format_with_timezone = timezone_datetime.isoformat()
+print(iso_format_with_timezone)
 
+import time
+current_time = time.time()
+print(f"Поточний час: {current_time}") #Метод time.time() повертає поточний час у секундах з 1 січня 1970 року (epoch time).
 
+import time
+current_time = time.time()
+print(f"Поточний час: {current_time}")
+readable_time = time.ctime(current_time)
+print(f"Читабельний час: {readable_time}")
+
+import time
+current_time = time.time()
+print(f"Поточний час: {current_time}")
+local_time = time.localtime(current_time)
+print(f"Місцевий час: {local_time}")
+"""
+tm_year - рік
+tm_mon - місяць від 1 до 12
+tm_mday - день місяця від 1 до 31
+tm_hour - години від 0 до 23
+tm_min - хвилини від 0 до 59
+tm_sec - секунди від 0 до 59
+tm_wday - день тижня від 0 до 6
+tm_yday - день року від 1 до 366
+tm_isdst - прапорець літнього часу. 0 означає, що літній час не діє, -1 - інформація відсутня, 1 - літній час діє"""
 
