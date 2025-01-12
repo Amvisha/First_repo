a2ab550046306b8708ccad3de7c3d11899601862
Amvisha <amvisha@gmail.com>
2025-01-12 18:33:42 +0200

    9

--- Slices.py
+++ Slices.py
@@ -171,16 +171,39 @@ def second(size, **kwargs):
 print(first(5, "first", "second", "third"))  # Результат: 8
 print(first(1, "Alex", "Boris"))             # Результат: 3
 print(second(3, comment_one="first", comment_two="second", comment_third="third"))  # Результат: 6
-print(second(10, comment_one="Alex", comment_two="Boris")) """
+print(second(10, comment_one="Alex", comment_two="Boris"))
 
 def factorial(n):
     if n < 2:
         return 1
     else:
         return n * factorial(n - 1)
-
-
 def number_of_groups(n, k):
     return factorial(n) / (factorial(n - k) * factorial(k))
 number_of_groups(50, 7)
+
+import datetime
+now = datetime.datetime.now()
+print(now)
+
+from datetime import datetime
+current_datetime = datetime.now()
+print(current_datetime.year)
+print(current_datetime.month)
+print(current_datetime.day)
+print(current_datetime.hour)
+print(current_datetime.minute)
+print(current_datetime.second)
+print(current_datetime.microsecond)
+print(current_datetime.tzinfo)
+print(current_datetime.date())
+print(current_datetime.time())"""
+
+import datetime
+# Створення об'єктів date і time
+date_part = datetime.date(2023, 12, 14)
+time_part = datetime.time(12, 30, 15)
+# Комбінування дати і часу в один об'єкт datetime
+combined_datetime = datetime.datetime.combine(date_part, time_part)
+print(combined_datetime)  # Виведе "2023-12-14 12:30:15"
 
