7b5c2d5315cc9e6c1b4729ef4d07c5207c3f75a5
Amvisha <amvisha@gmail.com>
2025-01-14 00:23:19 +0200

    9

--- args.py
+++ args.py
@@ -43,3 +43,29 @@ text = "Рік 2023 був складнішим, ніж 2022"
 pattern = r"\d+"
 matches = re.findall(pattern, text)
 print(matches)
+
+import re
+text = "Python - це проста, але потужна мова програмування."
+pattern = r"\w+"
+matches = re.findall(pattern, text)
+print(matches)  # Виведе список всіх слів у рядку
+
+import re
+text = "Контакти: example1@example.com, example2@sample.org"
+pattern = r"\w+@\w+\.\w+"
+matches = re.findall(pattern, text)
+print(matches)  # Виведе всі знайдені електронні адреси
+
+import re
+file_name = "Мій документ Python.txt"
+pattern = r"\s"
+replacement = "_"
+formatted_name = re.sub(pattern, replacement, file_name)
+print(formatted_name)
+
+import re
+text = "Python - потужна, універсальна; мова!"
+pattern = r"[;,\-:!\.]"
+replacement = ""
+modified_text = re.sub(pattern, replacement, text)
+print(modified_text)
