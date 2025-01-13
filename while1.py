f743425c89a5bab056c4b615cd1472af44a6d4ed
Amvisha <amvisha@gmail.com>
2025-01-13 23:29:48 +0200

    9

--- while1.py
+++ while1.py
@@ -145,5 +145,60 @@ for el in query.split('&'):
     obj_query.update({key: value.replace('+', ' ')})
 print(obj_query)
 
+for char in "Hello 123":
+    if char.isdigit():
+        print(f"'{char}' - це цифра")
+    else:
+        print(f"'{char}' - не цифра")
 
+intab = "aeiou"
+outtab = "12345"
+trantab = str.maketrans(intab, outtab)
+str = "This is string example"
+print(str.translate(trantab))
 
+symbols = "0123456789ABCDEF"
+code = [
+        '0000', '0001', '0010', '0011', '0100', '0101', '0110', '0111',
+        '1000', '1001', '1010', '1011', '1100', '1101', '1110', '1111'
+        ]
+MAP = {}
+for s, c in zip(symbols, code):
+    MAP[ord(s)] = c
+    MAP[ord(s.lower())] = c
+result = "34 DF 56 AC".translate(MAP)
+print(result)
+
+morze_dict = {'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.',
+              'G': '--.', 'H': '....', 'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..',
+              'M': '--', 'N': '-.', 'O': '---', 'P': '.--.', 'Q': '--.-', 'R': '.-.',
+              'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-',
+              'Y': '-.--', 'Z': '--..', '0': '-----', '1': '.----', '2': '..---',
+              '3': '...--', '4': '....-', '5': '.....', '6': '-....', '7': '--...',
+              '8': '---..', '9': '----.'}
+# Перетворення ключів словника на Unicode коди
+table_morze_dict = {}
+for k, v in morze_dict.items():
+    table_morze_dict[ord(k)] = v
+string = "Hello world"
+result = ""
+for ch in string:
+    result = result + ch.upper().translate(table_morze_dict)
+print(result)
+
+for i in range(8):
+    s = f"int: {i:d};  hex: {i:#x};  oct: {i:#o};  bin: {i:#b}"
+    print(s)
+
+price = 19.99
+quantity = 3
+total = f"Total: {price * quantity:.2f}"
+print(total)
+
+width = 5
+for num in range(12):
+    print(f'{num:^6} {num**2:^6} {num**3:^6}')
+
+name = "Alice"
+formatted = f"{name:>10}"
+print(formatted)  # Виведе: '     Alice' (вирівнювання праворуч)
