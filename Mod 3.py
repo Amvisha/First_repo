c224dfb7bc9a87eadc9286a224cdb6860259b549
Amvisha <amvisha@gmail.com>
2025-01-16 02:30:45 +0200

    9

--- Mod 3.py
+++ Mod 3.py
@@ -49,6 +49,7 @@ next_friday = find_next_weekday(start_date, 4)  # Знайти наступну 
 print(next_friday)
 """
 
+"""
 from datetime import datetime, date
 
 
@@ -89,3 +90,88 @@ for user in users:
 
 upcoming_birthdays = get_upcoming_birthdays(birthday_this_year, days=7)
 print(upcoming_birthdays)
+"""
+
+from datetime import datetime, date
+
+
+def string_to_date(date_string):
+    return datetime.strptime(date_string, "%Y.%m.%d").date()
+
+def date_to_string(date):
+    return date.strftime("%Y.%m.%d")
+
+def prepare_user_list(user_data):
+    prepared_list = []
+    for user in user_data:
+        prepared_list.append({"name": user["name"], "birthday": string_to_date(user["birthday"])})
+    return prepared_list
+
+def get_upcoming_birthdays(users, days=7):
+    upcoming_birthdays = []
+    today = datetime(year=2024, month=4, day=22).date()
+    for user in users:
+        if (user["birthday"] - today).days >= 0 & (user["birthday"] - today).days <= days:
+            upcoming_birthdays.append({"name": user["name"], "congratulation_date": date_to_string(user["birthday"])})
+    return upcoming_birthdays
+
+users = [
+    {"name": "Bill Gates", "birthday": "1955.4.23"},
+    {"name": "Steve Jobs", "birthday": "1955.4.25"},
+    {"name": "Jinny Lee", "birthday": "1956.3.22"},
+    {"name": "John Doe", "birthday": "1985.01.23"},
+    {"name": "Jane Smith", "birthday": "1990.01.27"}
+]
+prepared_users = prepare_user_list(users)
+
+#today = string_to_date("2024.04.22")
+birthday_this_year = []
+today = datetime(year=2024, month=4, day=22).date()
+print(today)
+for user in users:
+    birthday_this_year.append({"name": user["name"], "birthday": string_to_date(user["birthday"]).replace(year=2024)})
+
+upcoming_birthdays = get_upcoming_birthdays(birthday_this_year, days=7)
+print(upcoming_birthdays)
+
+from datetime import datetime, date
+
+
+def string_to_date(date_string):
+    return datetime.strptime(date_string, "%Y.%m.%d").date()
+
+
+def date_to_string(date):
+    return date.strftime("%Y.%m.%d")
+
+
+def prepare_user_list(user_data):
+    prepared_list = []
+    for user in user_data:
+        prepared_list.append({"name": user["name"], "birthday": string_to_date(user["birthday"])})
+    return prepared_list
+
+
+def get_upcoming_birthdays(users, days=7):
+    upcoming_birthdays = []
+    today = date.today()
+    for user in users:
+        if (user["birthday"] - today).days >= 0 and (user["birthday"] - today).days <= days:
+            upcoming_birthdays.append({"name": user["name"], "congratulation_date": date_to_string(user["birthday"])})
+    return upcoming_birthdays
+
+
+users = [
+    {"name": "Sarah Lee", "birthday": "1957.03.30"},
+    {"name": "John Doe", "birthday": "1985.03.28"},
+    {"name": "Jane Smith", "birthday": "1990.03.27"},
+    {"name": "John Doe", "birthday": "1985.01.23"},
+]
+birthday_this_year = prepare_user_list(users)
+
+today = date.today()
+for user in users:
+    birthday_this_year.append(
+        {"name": user["name"], "birthday": string_to_date(user["birthday"]).replace(year=today.year)})
+
+print(get_upcoming_birthdays(birthday_this_year))
