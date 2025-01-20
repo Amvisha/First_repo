580b62057b28ba6847319a8e940a0f8acd5f5bcc
Amvisha <amvisha@gmail.com>
2025-01-20 23:42:13 +0200

    9

--- lists.py
+++ lists.py
@@ -1,3 +1,5 @@
+from pygame.draw import circle
+
 my_list = [1, 2, 3, 5, 7, 346, 235, 235, 347, -435, -23, 0]
 
 """print(my_list.index(347))
@@ -32,7 +34,7 @@ users = [
 ]
 print(users[:]["birthday"]
 """
-
+'''
 my_set = {1, 2, 3, 56, 'test', 'Test'}
 print(my_set)
 my_set.add('fakell')
@@ -48,3 +50,27 @@ print(list(set(list_one) & set(list_two))) # in fist and second & (and)
 print(list(set(list_one) | set(list_two))) # in fist or in second | (or)
 print(list(set(list_one) - set(list_two))) # in fist and not in second & (minus)
 print(list(set(list_one) ^ set(list_two))) # in fist and not in second + in second and not in fist ^
+list_one = [1, 1, 2, 3, 5, 8, 13]
+list_two = [11, 1, 21, 31, 15, 8]
+print(len(list_one) == len(set(list_one)))
+print(len(list_two) == len(set(list_two)))
+
+circle = {
+    (0, 0): "Centre",
+    (0, 1): "90",
+    (1, 0): "0 or 360",
+    (0, -1): "270",
+    (-1, 0): "180",
+}
+print(circle.get((0, -1))).
+'''
+
+my_string = 'hello my LiTTle friends!'
+print(my_string.upper()) # HELLO MY LITTLE FRIENDS!
+print(my_string.lower()) # hello my little friends!
+print(my_string.startswith('hello')) # True
+print(my_string.startswith('hi')) # False
+print(my_string.endswith('!')) # True
+print(my_string.endswith('.')) # False
+print(my_string.title()) # Hello My Little Friends!
+print(my_string.count('e')) # 3
