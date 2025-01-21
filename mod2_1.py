c6902b7051c6396724da687aa7a0f61c4c58b8f0
Amvisha <amvisha@gmail.com>
2025-01-21 02:24:56 +0200

    9

--- mod2_1.py
+++ mod2_1.py
@@ -15,3 +15,23 @@ for char in text:
     count += 1
     dict_counter[char] = count # записуємо значення по ключу
 print(dict_counter)
+
+text = "What is Lorem Ipsum? \
+Lorem Ipsum is simply dummy text of the printing and typesetting industry.\
+ Lorem Ipsum has been the industry('s standard dummy text ever since the 1500s, \
+ when an unknown printer took a galley of type and scrambled it to make a type \
+ specimen book. It has survived not only five centuries, but also the leap into \
+ electronic typesetting, remaining essentially unchanged.\
+  It was popularised in the 1960s with the release of Letraset sheets containing\
+   Lorem Ipsum passages, and more recently with desktop publishing software like \
+   Aldus PageMaker including versions of Lorem Ipsum."
+char_set = set()
+symbol_set = set()
+
+for el in text:
+    if el.lower() in alphabet:
+        char_set.add(el)
+    else:
+        symbol_set.add(el)
+print(f'Chars {char_set}')
+print(f'Symbols {symbol_set}')
